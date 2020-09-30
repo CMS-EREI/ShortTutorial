f590f41c70e75884897317e9cb31c135544292a1
Developer2CMSMainz <erik.reisbeck@cms-mainz.de>
2020-09-30 10:09:49 +0200

    Testing git

---
+++ src/main/java/com/example/restfulservice/Test.java
@@ -0,0 +1,12 @@
+package com.example.restfulservice;
+
+public class Test {
+	private final String content;
+	
+	public Test(String content) {
+		this.content = content;
+	}
+	public String getContent(){
+		return content;
+	}
+}
